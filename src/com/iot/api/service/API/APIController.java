3cb23778584ce2887f82eea1c8414f90fb4cc8b9
Walid <walidarbel77@gmail.com>
2018-10-29 14:27:45 +0100

    Classe Controller API

---
+++ src/com/iot/api/service/API/APIController.java
@@ -0,0 +1,10 @@
+package com.iot.api.service.API;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class APIController {
+
+}
